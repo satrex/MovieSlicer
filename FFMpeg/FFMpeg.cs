7ba9ff0f0a58b67d1114cab05ba41ff3d1e29ba5
satrex <info@satrex.jp>
2018-09-12 10:49:22 +0900

    FFMpegプロジェクトを追加

--- FFMpeg/FFMpeg.cs
+++ FFMpeg/FFMpeg.cs
@@ -1,8 +1,43 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Satrex.FFMpeg
 {
-    public static class 
+    public class FFMpeg
     {
+        public static string ExecutablePath{
+            get;
+            set;
+        }
+
+        public FFMpeg()
+        {
+            if(ExecutablePath == null){
+                SetExcutablePath();
+            }
+
+        }
+
+        private void SetExcutablePath()
+        {
+            ProcessStartInfo psInfo = new ProcessStartInfo();
+            psInfo.FileName = @"/Applications/Utilities/Terminal.app/Contents/MacOS/Terminal"; // 実行するファイル
+            psInfo.Arguments = "which FFMpeg";
+            psInfo.CreateNoWindow = false; // コンソール・ウィンドウを開かない
+
+            psInfo.UseShellExecute = false; // シェル機能を使用しない
+            psInfo.RedirectStandardOutput = true;
+
+            var p = Process.Start(psInfo);
+
+            p.EnableRaisingEvents = true;
+            p.OutputDataReceived += (sender, e) => ExecutablePath = e.Data;
+            p.BeginOutputReadLine();
+            p.WaitForExit();
+        }
+
+
+
+
     }
 }
